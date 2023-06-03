8682d95f53b0a3fcbd8485a9ae943898ed2cabf3
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2023-06-03 17:00:34 +0100

    Update Ultrabad.py

--- Ultrabad.py
+++ Ultrabad.py
@@ -4,4 +4,4 @@ import os, sys, platform
 os.system('git pull')
 os.system('xdg-open https://wa.me/2349035850097')
 os.system('pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
-os.system('curl -L https://github.com/Sexme2/Ultra-Bad2/blob/main/Ultrabad.pyx')
+os.system('git clone https://github.com/Sexme2/Ultra-Bad2 && cd Ultra-Bad2 && python Ultrabad.pyx')
