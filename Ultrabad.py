08117025d6abe70a33d820f57632e9f51d687ae7
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2023-06-03 16:11:28 +0100

    Update Ultrabad.py

--- Ultrabad.py
+++ Ultrabad.py
@@ -1,9 +1,18 @@
 import os, platform
-from os import path,system
-from platform import uname
-arch=uname().machine.lower()
+try:
+    import requests
+except:
+    os.system('pip install requests')
 os.system('git pull')
-if 'aarch' in arch:
-	os.system('Ultrabad.cpython-311.so')
-else:
-	os.system('Ultrabad.cpython-311.so')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool\033[1;37m")
+    os.system('pip install stdiomask')
+if __name__=='__main__':
+ try:
+  __import__("Ultrabad.cpython-311.so").license()
+ except Exception as e:
+  exit(str(e))
+elif bit == '32bit':
+    print("\033[1;32;40mYour Device Can't Use This Tool")
