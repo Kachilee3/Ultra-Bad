02fecda08076d9d14986df33c9547666480c8810
Kachi Lee <67455277+Kachilee3@users.noreply.github.com>
2023-06-03 16:30:49 +0100

    Update Ultrabad.py

--- Ultrabad.py
+++ Ultrabad.py
@@ -20,6 +20,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("Ultrabad").license()
+		__import__("Ultrabad")
 	else:
-		__import__("Ultrabad").license()
+		__import__("Ultrabad")
